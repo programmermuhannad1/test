5bb95418437ba390dfd3067db8b18e74399947e2
programmermuhannad1 <oplkn99@gmail.com>
2025-03-07 13:26:00 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,16 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 5173,  // يمكنك تحديد المنفذ الذي تريد تشغيله محليًا
+    open: true,  // لفتح المتصفح تلقائيًا عند تشغيل المشروع
+  },
+  build: {
+    outDir: 'dist',  // تحديد مجلد البناء
+  },
+  // هذا الاختيار يضمن أن Vercel يحدد بشكل صحيح بيئة الإنتاج
+  base: '/',
+})
