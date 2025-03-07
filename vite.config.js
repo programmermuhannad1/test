6196d3b828032f42286896fa84281c31326f6e76
programmermuhannad1 <oplkn99@gmail.com>
2025-03-07 15:52:45 +0300

    Update: Your changes here

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    port: 5173,  // يمكنك تحديد المنفذ الذي تريد تشغيله محليًا
-    open: true,  // لفتح المتصفح تلقائيًا عند تشغيل المشروع
-  },
   build: {
     outDir: 'dist',  // تحديد مجلد البناء
     terserOptions: {
@@ -15,7 +10,7 @@ export default defineConfig({
         drop_console: true,  // لإزالة الـ console.logs في ملفات الإنتاج
       },
     },
-    assetsDir: 'assets',  // لتحديد مجلد الصور والملفات الثابتة
   },
-  base: process.env.NODE_ENV === 'production' ? '/front/' : '/',  // تغيير base حسب البيئة
+  base: '/',
+
 })
