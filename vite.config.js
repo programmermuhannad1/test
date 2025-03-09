36338d9bc52d2690c09ccc8ffac95835c5e78aa8
programmermuhannad1 <oplkn99@gmail.com>
2025-03-09 06:23:50 +0300

    Fix Vercel refresh issue

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    historyApiFallback: true
-  },
   build: {
     outDir: 'dist'
+  },
+  server: {
+    historyApiFallback: true
   }
 })
